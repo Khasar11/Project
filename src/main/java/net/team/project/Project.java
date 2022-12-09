a41d0d0fa7dad4674863ce310643c1fe52eeb3b3
Khasar <khasar@khasar.net>
2022-12-09 17:56:12 +0100

    Add vaultAPI ++ registration on start

--- src/main/java/net/team/project/Project.java
+++ src/main/java/net/team/project/Project.java
@@ -4,22 +4,20 @@ import java.util.logging.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Project extends JavaPlugin {
-    private static Project project;
+    private Project project;
     private final CommandInitializer commandInit = new CommandInitializer(project);
     public Logger logger = Logger.getLogger(this.getName());
+    private final VaultInitializer vaultInitializer = new VaultInitializer(project);
 
     @Override
     public void onEnable() {
         project = this;
         commandInit.initializeCommands();
+        vaultInitializer.setup();
     }
 
     @Override
     public void onDisable() {
 
-    }
-
-    public static Project getPlugin() {
-        return project;
     }
 }
