4ef0e974c399deb1c8e5b709efcaec459a13a4a3
Khasar <khasar@khasar.net>
2022-12-18 12:56:50 +0100

    Added main plugin enable and a check for it

--- src/main/java/net/team/project/Project.java
+++ src/main/java/net/team/project/Project.java
@@ -17,27 +17,29 @@ public final class Project extends JavaPlugin {
     @Override
     public void onEnable() {
         project = this;
-        logger = getLogger();
+        if (cfh.main.getConfig().getBoolean("enabled")) {
+            logger = getLogger();
 
-        CommandRegistrator commandRegistrator = new CommandRegistrator(project);
-        EventRegistrator eventRegistrator = new EventRegistrator(project);
-        VaultInitializer vaultInitializer = new VaultInitializer(project);
+            CommandRegistrator commandRegistrator = new CommandRegistrator(project);
+            EventRegistrator eventRegistrator = new EventRegistrator(project);
+            VaultInitializer vaultInitializer = new VaultInitializer(project);
 
-        cfh.initializeConfigurations();
+            cfh.initializeConfigurations();
 
-        // Register the plugin as an economy provider for vault
-        project.getServer().getServicesManager().register(Economy.class, new PEconomy(), this, ServicePriority.Highest);
+            // Register the plugin as an economy provider for vault
+            project.getServer().getServicesManager().register(Economy.class, new PEconomy(), this, ServicePriority.Highest);
 
-        commandRegistrator.initializeCommands();
-        eventRegistrator.initializeEvents();
-        //vaultInitializer.setup();
+            commandRegistrator.initializeCommands();
+            eventRegistrator.initializeEvents();
+            //vaultInitializer.setup();
 
-        logger.info(cfh.messages.getConfig().getString("startup-readout"));
+            logger.info(cfh.messages.getConfig().getString("startup"));
+        } else logger.warning(cfh.messages.getConfig().getString("disabled"));
     }
 
     @Override
     public void onDisable() {
-        logger.info(cfh.messages.getConfig().getString("disable-readout"));
+        logger.info(cfh.messages.getConfig().getString("disable"));
     }
 
     public static Project getInstance() { return project; }
